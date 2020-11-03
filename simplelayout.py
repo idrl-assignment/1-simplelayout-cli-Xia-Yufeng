b166448d0f6282fd9b843c559c23182be3066023
xia-yufeng <714811246@qq.com>
2020-11-03 16:01:37 +0800

    latest

--- simplelayout.py
+++ simplelayout.py
@@ -4,17 +4,19 @@ import scipy.io as scio
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--board_grid', help='board_grid')
-parser.add_argument('--unit_grid', help='unit_grid')
-parser.add_argument('--unit_n', help='unit_n')
-parser.add_argument('--positions', help='learning rate decay')
-parser.add_argument('--outdir', help='outdir')
-parser.add_argument('--file_name', help='training epochs')
+parser.add_argument('--board_grid', type=int, help='board_grid')
+parser.add_argument('--unit_grid', type=int, help='unit_grid')
+parser.add_argument('--unit_n', type=int, help='unit_n')
+parser.add_argument('--positions', type=int, help='learning rate decay')
+parser.add_argument('--outdir', type=str, help='outdir')
+parser.add_argument('--file_name', type=str, help='training epochs')
 args = parser.parse_args()
 
 def main():
     data = {'f': np.random.randint(0, 2, (args.board_grid, args.board_grid))}
+    # data = {'f': np.random.randint(0, 2, (100, 100))}
     path = args.outdir + '/' + args.file_name
+    # path = 'example'
     scio.savemat(path + '.mat', data)
     plt.imshow(data['f'])
     plt.savefig(path + '.jpg')
