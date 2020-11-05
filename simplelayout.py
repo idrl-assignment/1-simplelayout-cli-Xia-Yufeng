e1af61392ec516061959fc4eb0627a282c77fbee
xia-yufeng <714811246@qq.com>
2020-11-05 15:48:25 +0800

    latest

--- simplelayout.py
+++ simplelayout.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 from pathlib import Path
@@ -21,11 +22,14 @@ def main():
         sys.exit('User exit')
     if max(args.positions) > (args.board_grid / args.unit_grid)**2:
         sys.exit('User exit')
-
-    file1 = Path(args.outdir + '/' + args.file_name + '.mat')
-    file1.touch(exist_ok=True)
-    file2 = Path(args.outdir + '/' + args.file_name + '.jpg')
-    file2.touch(exist_ok=True)
+    
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+    filename = args.outdir + '/' + args.file_name
+    with open(filename + '.mat', 'w') as file_object:
+        pass
+    with open(filename + '.jpg', 'w') as file_object:
+        pass
 
 
 if __name__ == "__main__":
