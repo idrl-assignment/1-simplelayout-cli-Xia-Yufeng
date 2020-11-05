09b44d569e85534fff6c957467d19c694a3f8aea
xia-yufeng <714811246@qq.com>
2020-11-05 12:49:26 +0800

    latest

--- simplelayout.py
+++ simplelayout.py
@@ -21,8 +21,6 @@ def main():
         sys.exit('User exit')
     if max(args.positions) > (args.board_grid / args.unit_grid)**2:
         sys.exit('User exit')
-    path = Path(args.outdir)
-    path.mkdir()
 
     file1 = Path(args.outdir + '/' + args.file_name + '.mat')
     file1.touch(exist_ok=True)
